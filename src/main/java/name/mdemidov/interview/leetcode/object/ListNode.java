7ea19a03c92a149877a207f4507565cab3b57a82
Maksim Demidov <maxim.demidov@gmail.com>
2019-10-06 12:47:11 -0700

    ListNode(int x) constructor replaced with ListNode(int... values)

--- src/main/java/name/mdemidov/interview/leetcode/object/ListNode.java
+++ src/main/java/name/mdemidov/interview/leetcode/object/ListNode.java
@@ -5,8 +5,12 @@ public class ListNode {
     public int val;
     public ListNode next;
 
-    public ListNode(int x) {
-        val = x;
+    public ListNode(int... values) {
+        if (values.length < 1) {
+            throw new IllegalArgumentException("Empty list of values");
+        }
+        val = values[0];
+        next = append(values, 1);
     }
 
     @Override
@@ -15,5 +19,16 @@ public class ListNode {
                "val=" + val +
                (next == null ? "" : ", next=" + next) +
                '}';
+    }
+
+    private ListNode append(int[] values, int startIndex) {
+        if (startIndex >= values.length) {
+            return null;
+        }
+        ListNode node = new ListNode(values[startIndex]);
+        if (startIndex < values.length - 1) {
+            node.next = append(values, startIndex + 1);
+        }
+        return node;
     }
 }
