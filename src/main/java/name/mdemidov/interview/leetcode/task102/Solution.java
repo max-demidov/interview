c435bcf71d3ba6b8f1b1fdceb9cdb5b4cf952061
Maksim Demidov <maxim.demidov@gmail.com>
2019-12-24 16:04:36 -0800

    task102 Runtime: 1 ms, faster than 85.17% Memory Usage: 36.3 MB, less than 100.00%

---
+++ src/main/java/name/mdemidov/interview/leetcode/task102/Solution.java
@@ -0,0 +1,69 @@
+package name.mdemidov.interview.leetcode.task102;
+
+import name.mdemidov.interview.leetcode.object.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/
+ *
+ * 102. Binary Tree Level Order Traversal
+ *
+ * Given a binary tree, return the level order traversal of its nodes' values.
+ * (ie, from left to right, level by level).
+ *
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * return its level order traversal as:
+ * [
+ * [3],
+ * [9,20],
+ * [15,7]
+ * ]
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        TreeNode tree1 = new TreeNode(3);
+        tree1.left = new TreeNode(9);
+        tree1.right = new TreeNode(20);
+        tree1.right.left = new TreeNode(15);
+        tree1.right.right = new TreeNode(7);
+        System.out.println(new Solution().levelOrder(tree1));
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int size = queue.size();
+            while (size-- > 0) {
+                TreeNode node = queue.remove();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(list);
+        }
+        return res;
+    }
+
+}
