025f5d99db0f4ed345ed0e0991b81fabb1d67492
Maksim Demidov <maxim.demidov@gmail.com>
2020-04-08 19:44:08 -0700

    MergeSort update

--- src/main/java/name/mdemidov/interview/algorithm/sort/merge/MergeSort.java
+++ src/main/java/name/mdemidov/interview/algorithm/sort/merge/MergeSort.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class MergeSort {
 
     public static void main(String[] args) {
-        int arr[] = {38, 12, 27, 11, 43, 13, 3, 5, 9, 6, 82, 7, 10};
-        System.out.println("Input: " + Arrays.toString(arr));
+        int arr[] = {38, 12, 82, 27, 11, 43, 13, 3, 5, 9, 6, 82, 7, 3, 10};
+        System.out.println("Input:  " + Arrays.toString(arr));
         sort(arr);
         System.out.println("Output: " + Arrays.toString(arr));
     }
@@ -15,30 +15,30 @@ public class MergeSort {
      * Sorts an array using Merge Sort algorithm.
      */
     public static void sort(int[] arr) {
-        sort(arr, 0, arr.length - 1);
+        sort(arr, 0, arr.length);
     }
 
     /**
-     * Sorts subarray arr[l..r] using merge().
+     * Sorts subarray arr[l..r-1] using merge().
      */
     private static void sort(int[] arr, int l, int r) {
-        if (l >= r) {
+        if (r - l < 2) {
             return;
         }
         int m = (l + r) / 2;
         sort(arr, l, m);
-        sort(arr, m + 1, r);
+        sort(arr, m, r);
         merge(arr, l, m, r);
     }
 
     /**
      * Merges two subarrays of arr[].
-     * First subarray is arr[l..m].
-     * Second subarray is arr[m+1..r]
+     * First subarray is arr[l..m-1].
+     * Second subarray is arr[m..r]
      */
     private static void merge(int[] arr, int l, int m, int r) {
         // Find sizes of two subarrays to be merged
-        int n1 = m - l + 1;
+        int n1 = m - l;
         int n2 = r - m;
 
         // Create temp arrays and copy data
@@ -48,7 +48,7 @@ public class MergeSort {
             L[i] = arr[l + i];
         }
         for (int i = 0; i < n2; i++) {
-            R[i] = arr[m + 1 + i];
+            R[i] = arr[m + i];
         }
 
         // Merge the temp arrays
@@ -56,26 +56,15 @@ public class MergeSort {
         int j = 0;
         int k = l;
         while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
-            } else {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
+            arr[k++] = L[i] < R[j] ? L[i++] : R[j++];
         }
 
         // Copy remaining elements if any
         while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
+            arr[k++] = L[i++];
         }
         while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
+            arr[k++] = R[j++];
         }
     }
 }
