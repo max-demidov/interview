9078c9827362c098411cb65231a526183b8f0f55
Maksim Demidov <maxim.demidov@gmail.com>
2021-01-24 18:08:45 -0800

    task0020
    Runtime: 1 ms, faster than 98.63%
    Memory Usage: 37.4 MB, less than 31.45%

--- src/main/java/name/mdemidov/interview/leetcode/task0020/Solution.java
+++ src/main/java/name/mdemidov/interview/leetcode/task0020/Solution.java
@@ -1,26 +1,41 @@
 package name.mdemidov.interview.leetcode.task0020;
 
+import java.util.Stack;
+
 /**
- * Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
- * determine if the input string is valid.
- * An input string is valid if:
- * 1. Open brackets must be closed by the same type of brackets.
- * 2. Open brackets must be closed in the correct order.
- * Note that an empty string is also considered valid.
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the
+ * input string is valid. An input string is valid if: 1. Open brackets must be closed by the same
+ * type of brackets. 2. Open brackets must be closed in the correct order. Note that an empty string
+ * is also considered valid.
  */
 public class Solution {
 
-    private static final String REGEX = "\\(\\)|\\[]|\\{}";
+  private static final Solution S = new Solution();
 
-    private static String cut(String s) {
-        String newS = s.replaceAll(REGEX, "");
-        if (newS.isEmpty() || newS.equals(s)) {
-            return newS;
+  public boolean isValid(String s) {
+    Stack<Character> stack = new Stack<>();
+    for (char c : s.toCharArray()) {
+      if (c == '{') {
+        stack.push('}');
+      } else if (c == '[') {
+        stack.push(']');
+      } else if (c == '(') {
+        stack.push(')');
+      } else {
+        if (stack.isEmpty() || c != stack.pop()) {
+          return false;
         }
-        return cut(newS);
+      }
     }
+    return stack.isEmpty();
+  }
 
-    public boolean isValid(String s) {
-        return cut(s).isEmpty();
-    }
+  public static void main(String[] args) {
+    System.out.println(S.isValid("()")); // true
+    System.out.println(S.isValid("()[]{}")); // true
+    System.out.println(S.isValid("(]")); // false
+    System.out.println(S.isValid("([)]")); // false
+    System.out.println(S.isValid("{[]}")); // true
+    System.out.println(S.isValid("]")); // false
+  }
 }
