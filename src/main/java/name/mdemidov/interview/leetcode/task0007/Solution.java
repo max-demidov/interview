b01f8be18d641c366767c49f6aa6c6a050b66ecf
Maksim Demidov <maxim.demidov@gmail.com>
2021-01-24 19:14:04 -0800

    task0007
    Runtime: 1 ms, faster than 100.00%
    Memory Usage: 36 MB, less than 71.11%

--- src/main/java/name/mdemidov/interview/leetcode/task0007/Solution.java
+++ src/main/java/name/mdemidov/interview/leetcode/task0007/Solution.java
@@ -2,20 +2,28 @@ package name.mdemidov.interview.leetcode.task0007;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        System.out.println(new Solution().reverse(-2147483648));
-    }
+  private static final Solution S = new Solution();
 
-    public int reverse(int x) {
-        int res = 0;
-        for (; x != 0 || x / 10 != 0; x /= 10) {
-            System.out.println("x=" + x);
-            if (res > Integer.MAX_VALUE / 10 || res < Integer.MIN_VALUE / 10) {
-                return 0;
-            }
-            res = res * 10 + x % 10;
-            System.out.println("res=" + res);
-        }
-        return res;
+  public int reverse(int x) {
+    int res = 0;
+    int max = Integer.MAX_VALUE / 10;
+    int min = Integer.MIN_VALUE / 10;
+    while (x != 0) {
+      if (res > max || res < min) {
+        return 0;
+      }
+      res = res * 10 + x % 10;
+      x /= 10;
     }
+    return res;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(S.reverse(123)); // 312
+    System.out.println(S.reverse(-123)); // -321
+    System.out.println(S.reverse(120)); // 21
+    System.out.println(S.reverse(0)); // 0
+    System.out.println(S.reverse(Integer.MAX_VALUE)); // 0
+    System.out.println(S.reverse(Integer.MIN_VALUE)); // 0
+  }
 }
